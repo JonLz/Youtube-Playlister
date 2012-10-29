062b5886a64441470b180699bb08053da7afa0a3
JonLz <jon.lazar@gmail.com>
2012-10-28 21:52:37 -0400

    Base site outline, devise, bootstrap, tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pictorealm::Application.config.secret_token = 'aa4a0cfe9078b18048ae7c64820d935fff4a50be9392ad67873a9027c037aa3648d4b0a5ab29c0fecd7c2e945c54c368c5104b526ed636183fe440f131d6dbe1'
