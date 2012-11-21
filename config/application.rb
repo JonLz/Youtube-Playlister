c0ae2e3b17a9d52d6d5a8c20b2a55bec772572ce
JonLz <jon.lazar@gmail.com>
2012-11-20 22:58:43 -0500

    Fix production env

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,9 @@ module Pictorealm
     # Enable the asset pipeline
     config.assets.enabled = true
 
+     # Added to fix heroku errors
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
