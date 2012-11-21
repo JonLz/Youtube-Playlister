3d2ac33dd9b6424ba710e56f39c9d5509da508bf
JonLz <jon.lazar@gmail.com>
2012-11-20 23:22:50 -0500

    vendor assets compiled

--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,7 @@ module Pictorealm
     config.assets.enabled = true
 
      # Added to fix heroku errors
+     
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
